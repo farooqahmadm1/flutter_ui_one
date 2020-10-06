8120a33c5cc2c6af228de45b7f80a9100dc1512a
Farooq.Ahmad <farooq.ahmad7@ibex.co>
2020-10-06 20:10:07 +0500

    Ui Design only

---
+++ android/app/src/main/kotlin/com/farooqdev/flutter_ui_one/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.farooqdev.flutter_ui_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home : HomePage(),
+  )
+);
+
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          brightness: Brightness.light,
+          leading: IconButton(
+            icon: Icon(Icons.menu, color: Colors.black87,),
+            onPressed: (){},
+          ),
+      ),
+      body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(bottom: Radius.circular(30.0))
+                ),
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Find Your',style:TextStyle(color: Colors.black87,fontSize:25)),
+                    SizedBox(height: 5),
+                    Text('Inspiration',style:TextStyle(color: Colors.black87,fontSize:40)),
+                    SizedBox(height:20),
+                    Container(
+                      padding: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(244, 243, 243, 1),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search,color:Colors.black87),
+                          hintText: "Search you're looking for",
+                          hintStyle: TextStyle(color: Colors.grey,fontSize: 15)
+                        ),
+                      ),
+                    )
+                  ]
+                ),
+              )
+          ]),
+      ),
+    );
+  }
+}
