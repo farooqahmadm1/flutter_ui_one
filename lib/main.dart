7fdceb35c967e5b99d078e1cb1101ec7526c50a7
Farooq.Ahmad <farooq.ahmad7@ibex.co>
2020-10-07 10:50:20 +0500

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home : HomePage(),
-  )
-);
-
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -16,57 +14,149 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(244, 243, 243, 1),
       appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          brightness: Brightness.light,
-          leading: IconButton(
-            icon: Icon(Icons.menu, color: Colors.black87,),
-            onPressed: (){},
+        backgroundColor: Colors.white,
+        elevation: 0,
+        brightness: Brightness.light,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black87,
           ),
+          onPressed: () {},
+        ),
       ),
       body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(bottom: Radius.circular(30.0))
-                ),
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Find Your',style:TextStyle(color: Colors.black87,fontSize:25)),
-                    SizedBox(height: 5),
-                    Text('Inspiration',style:TextStyle(color: Colors.black87,fontSize:40)),
-                    SizedBox(height:20),
-                    Container(
-                      padding: EdgeInsets.all(5.0),
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(244, 243, 243, 1),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+            Widget>[
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30.0))),
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Find Your',
+                      style: TextStyle(color: Colors.black87, fontSize: 25)),
+                  SizedBox(height: 5),
+                  Text('Inspiration',
+                      style: TextStyle(color: Colors.black87, fontSize: 40)),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(244, 243, 243, 1),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
                           border: InputBorder.none,
-                          prefixIcon: Icon(Icons.search,color:Colors.black87),
+                          prefixIcon: Icon(Icons.search, color: Colors.black87),
                           hintText: "Search you're looking for",
-                          hintStyle: TextStyle(color: Colors.grey,fontSize: 15)
-                        ),
+                          hintStyle:
+                              TextStyle(color: Colors.grey, fontSize: 15)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  )
+                ]),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Promo Today',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        promoCard(AssetImage('assests/images/four.jpg')),
+                        promoCard(AssetImage('assests/images/two.jpg')),
+                        promoCard(AssetImage('assests/images/three.jpg')),
+                        promoCard(AssetImage('assests/images/one.jpg')),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assests/images/three.jpg'))),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomRight,
+                            stops: [
+                              0.1,
+                              0.8
+                            ],
+                            colors: [
+                              Colors.black.withOpacity(.8),
+                              Colors.black.withOpacity(.1)
+                            ]),
                       ),
-                    )
-                  ]
-                ),
-              )
-          ]),
+                      child: Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: EdgeInsets.all(16),
+                            child: Text(
+                              'Best Design',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          )),
+                    ),
+                  ),
+                ]),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Widget promoCard(image) {
+    return AspectRatio(
+      aspectRatio: 2.4 / 3,
+      child: Container(
+        margin: EdgeInsets.only(right: 12.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(fit: BoxFit.cover, image: image)),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+              0.1,
+              0.8
+            ], colors: [
+              Colors.black.withOpacity(.8),
+              Colors.black.withOpacity(.1)
+            ]),
+          ),
+        ),
       ),
     );
   }
